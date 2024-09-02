b94dd70072ddf77b35784d844a648b22aea51917
DamodarPatil <patildamodar1983@gmail.com>
2024-09-02 17:54:11 +0530

    Add initial project files and configurations

---
+++ script.js
@@ -0,0 +1,98 @@
+{/* <section class="bg-white p-[100px]">
+      <div class="max-w-full m-auto pr-[15px]">
+        <div class="section-header text-center mb-[50px]">
+          <h2
+            class="title text-[35px] font-semibold capitalize text-gray-800 leading-tight"
+          >
+            Employee Testimonials
+          </h2>
+        </div>
+        <div class="testimonials-content">
+          <div class="swiper testimonials-slider js-testimonials-slider">
+            <div class="swiper-wrapper">
+              <div
+                class="swiper-slide testimonials-item bg-gray-100 p-[30px] rounded-[30px]"
+              >
+                <div class="info">
+                  <img src="assets/profileimage.jpg" alt="img" />
+                  <div class="text-box">
+                    <h3 class="name">John wick</h3>
+                    <span class="job">web developer</span>
+                  </div>
+                </div>
+                <p>
+                  Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                  Veritatis expedita dolores dicta, laborum officiis sed impedit
+                  eius laudantium animi pariatur!
+                </p>
+              </div>
+              <div
+                class="swiper-slide testimonials-item bg-gray-100 p-[30px] rounded-[30px]"
+              >
+                <div class="info">
+                  <img src="assets/profileimage.jpg" alt="img" />
+                  <div class="text-box">
+                    <h3 class="name">John wick</h3>
+                    <span class="job">web developer</span>
+                  </div>
+                </div>
+                <p>
+                  Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                  Veritatis expedita dolores dicta, laborum officiis sed impedit
+                  eius laudantium animi pariatur!
+                </p>
+              </div>
+              <div
+                class="swiper-slide testimonials-item bg-gray-100 p-[30px] rounded-[30px]"
+              >
+                <div class="info">
+                  <img src="assets/profileimage.jpg" alt="img" />
+                  <div class="text-box">
+                    <h3 class="name">John wick</h3>
+                    <span class="job">web developer</span>
+                  </div>
+                </div>
+                <p>
+                  Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                  Veritatis expedita dolores dicta, laborum officiis sed impedit
+                  eius laudantium animi pariatur!
+                </p>
+              </div>
+              <div
+                class="swiper-slide testimonials-item bg-gray-100 p-[30px] rounded-[30px]"
+              >
+                <div class="info">
+                  <img src="assets/profileimage.jpg" alt="img" />
+                  <div class="text-box">
+                    <h3 class="name">John wick</h3>
+                    <span class="job">web developer</span>
+                  </div>
+                </div>
+                <p>
+                  Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                  Veritatis expedita dolores dicta, laborum officiis sed impedit
+                  eius laudantium animi pariatur!
+                </p>
+              </div>
+              <div
+                class="swiper-slide testimonials-item bg-gray-100 p-[30px] rounded-[30px]"
+              >
+                <div class="info">
+                  <img src="assets/profileimage.jpg" alt="img" />
+                  <div class="text-box">
+                    <h3 class="name">John wick</h3>
+                    <span class="job">web developer</span>
+                  </div>
+                </div>
+                <p>
+                  Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                  Veritatis expedita dolores dicta, laborum officiis sed impedit
+                  eius laudantium animi pariatur!
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="swiper-pagination js-testimonials-pagination"></div>
+        </div>
+      </div>
+    </section> */}
